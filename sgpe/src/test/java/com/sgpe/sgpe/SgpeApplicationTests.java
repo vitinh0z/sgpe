875f28e4f9b930325d17f06249a0bc9b8977b4f8
vitinh0z <victor10.vg23@gmail.com>
2026-02-27 18:58:47 -0300

    sei la oq é isso

---
+++ sgpe/src/test/java/com/sgpe/sgpe/SgpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgpe.sgpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

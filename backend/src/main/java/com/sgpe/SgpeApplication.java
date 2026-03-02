52f6f43515d11a252ebb714819adbc8e12ced11f
vitinh0z <victor10.vg23@gmail.com>
2026-03-02 15:06:52 -0300

    chore: configura projeto backend com Maven Wrapper e adiciona .gitignore

--- backend/src/main/java/com/sgpe/SgpeApplication.java
+++ backend/src/main/java/com/sgpe/SgpeApplication.java
@@ -1,5 +1,12 @@
 package com.sgpe;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SgpeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SgpeApplication.class, args);
+    }
 }
 

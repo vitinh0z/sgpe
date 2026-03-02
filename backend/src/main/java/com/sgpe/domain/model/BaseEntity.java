857ea931b4b54795b7be5f1948893373bf1a2c09
vitinh0z <victor10.vg23@gmail.com>
2026-03-02 18:21:22 -0300

    feat: implementa BaseEntity e Tenant Domain

--- backend/src/main/java/com/sgpe/domain/model/BaseEntity.java
+++ backend/src/main/java/com/sgpe/domain/model/BaseEntity.java
@@ -1,5 +1,35 @@
 package com.sgpe.domain.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public abstract class BaseEntity {
+
+    private UUID id;
+
+    private UUID tenantId;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+
+    private String cratedBy;
+
+
+
+
+
 }
 
--- backend/src/main/java/com/sgpe/domain/model/Tenant.java
+++ backend/src/main/java/com/sgpe/domain/model/Tenant.java
@@ -1,5 +1,29 @@
 package com.sgpe.domain.model;
 
+import com.sgpe.domain.enums.BusinessType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Tenant {
+
+    private UUID id;
+
+    private String businessName;
+
+    private String cnpj;
+
+    private BusinessType businessType;
+
+    private boolean active;
+
+    private String settingsJson;
 }
 

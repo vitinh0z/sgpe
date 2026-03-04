411cbc2d78e0ce413688173b72ea5fe3649611bb
vitinh0z <victor10.vg23@gmail.com>
2026-03-04 17:57:14 -0300

    feat: implementa entidade user

--- backend/src/main/java/com/sgpe/domain/user/entity/User.java
+++ backend/src/main/java/com/sgpe/domain/user/entity/User.java
@@ -1,8 +1,27 @@
 package com.sgpe.domain.user.entity;
 
 import com.sgpe.domain.constant.BaseEntity;
+import com.sgpe.domain.constant.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User extends BaseEntity {
+
+    private String name;
+
+    private String email;
+
+    private String passwordHash;
+
+    private String telephone;
+
+    private UserRole role;
+
+    private boolean active = true;
 }
-
-
